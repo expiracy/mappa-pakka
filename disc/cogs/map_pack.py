d2096d38a81c76481aacc6d68a065144062310f8
Phisyqx <tingyi_lu@hotmail.com>
2024-09-13 04:17:50 +0100

    dropbox client done statically various naming changes client is now called clients

--- disc/cogs/map_pack.py
+++ disc/cogs/map_pack.py
@@ -20,7 +20,7 @@ class MapPack(commands.Cog):
         if map_pack.stat().st_size > 25e6:
             await ctx.reply("The file(s) you have provided us with is larger than the upload limit for Discord, "
                             "please wait as we upload it to dropbox")
-            url = maps.upload("/mappapakka/") # TODO: Give these file uploads a nice name?
+            url = await maps.upload() 
             await ctx.reply(f"Your file resides in this link {url}")
         else:
             await ctx.reply(file=discord.File(map_pack))
